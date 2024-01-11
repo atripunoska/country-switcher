9596bcef9af7a3e0b1776566533d35ac2b8848ef
atripunoska <atripunoska@gmail.com>
2024-01-11 23:32:57 +0100

    feat: add Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="flex justify-between">
+      <div>Where in the world?</div>
+      <button>Dark mode</button>
+    </header>
+  );
+};
+
+export default Header;
